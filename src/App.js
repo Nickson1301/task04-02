c7ec924c4fed323e8fcb54376cabc8a277fe47c1
Sindhu Godson <sinthugodson1@gmail.com>
2026-02-04 22:50:23 +0530

    commented appjs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import RojarComp from './components/RojarComp';
-import NicksonComp from './components/NicksonComp';
-import CounterComp from './components/CounterComp';
+// import RojarComp from './components/RojarComp';
+// import NicksonComp from './components/NicksonComp';
+// import CounterComp from './components/CounterComp';
 import ToggleImage from './components/ToggleImage';
  import ChangeImage from './components/ChangeImage';
 
 function App() {
   return (
     <div className="App">
-      <RojarComp fname="Rojar" lname="Nickson" email="rojar@gmail.com" contact={9087685650} city="Chennai"/><hr></hr>
+      {/* <RojarComp fname="Rojar" lname="Nickson" email="rojar@gmail.com" contact={9087685650} city="Chennai"/><hr></hr>
       <NicksonComp fname="Rojar" lname="Nickson" email="rojar@gmail.com" contact={9087685650} city="Chennai"/><hr/>
-      <CounterComp/><hr></hr>
+      <CounterComp/><hr></hr> */}
 
       <ToggleImage/><hr></hr>
       <ChangeImage/>
